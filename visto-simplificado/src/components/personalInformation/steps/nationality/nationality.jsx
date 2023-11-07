7f5d1d80829891d3b5cc08d02de21fb1809c42a6
Diego Nobre <diegonobre.dev@outlook.com>
2023-11-07 17:34:17 -0300

    Criação input imagem

--- visto-simplificado/src/components/personalInformation/steps/nationality/nationality.jsx
+++ visto-simplificado/src/components/personalInformation/steps/nationality/nationality.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react"
 import './nationality.css'
-import {MenuItem, Select, TextField } from "@mui/material";
+import { MenuItem, Select, TextField } from "@mui/material";
 import statesBrazilianService from "../../../../services/statesBrazilianService"
-import {FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
 
-function Nationality () {
-    const getStates = async () =>{        
+function Nationality() {
+    const getStates = async () => {
         const response = await statesBrazilianService.getStates();
-        setStates(response);        
+        setStates(response);
     }
 
     const [states, setStates] = useState([]);
@@ -21,82 +22,82 @@ function Nationality () {
     const handleChangeRequester = (event) => {
         setRadioRequester(event.target.value);
     };
-    
+
     useEffect(() => {
         getStates();
     }, []);
 
-  return (        
+    return (
         <div className="div-margin">
             <div className="padding-bottom">
-                <div style={{display:'flex', justifyContent: 'space-between'}}>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <div>
                         <span className="title-header">Nacionalidade</span>
                     </div>
                 </div>
-                <hr className="hr-color"/>                
+                <hr className="hr-color" />
             </div>
             <div className="div-nationality-padding">
                 <div className="padding-bottom-nationality">
-                    <span className="title-header-2">Nacionalidade<span style={{color:'red'}}>*</span></span>
+                    <span className="title-header-2">Nacionalidade<span style={{ color: 'red' }}>*</span></span>
                 </div>
                 <div className="div-grid-nationality-inputs">
                     <div>
-                        <div style={{paddingBottom:'0.4rem'}}>
-                            <span className="span-state">Pais natal<span style={{color:'red'}}>*</span></span>
+                        <div style={{ paddingBottom: '0.4rem' }}>
+                            <span className="span-state">Pais natal<span style={{ color: 'red' }}>*</span></span>
                         </div>
                         <div className="padding-bottom-1">
                             <Select
-                            className="style-select-nationality"
-                            labelId="select-state"
-                            id="select-state"
-                            value={selectedState}
-                            onChange={handleChangeSelect}
+                                className="style-select-nationality"
+                                labelId="select-state"
+                                id="select-state"
+                                value={selectedState}
+                                onChange={handleChangeSelect}
                             >
                                 {states.map((state) => (
-                                <MenuItem key={state.id} value={state.nome}>
-                                    {state.nome}
-                                </MenuItem>
+                                    <MenuItem key={state.id} value={state.nome}>
+                                        {state.nome}
+                                    </MenuItem>
                                 ))}
                             </Select>
                         </div>
                     </div>
                     <div>
-                        <div style={{paddingBottom:'0.4rem'}}>
-                            <span className="span-state">Cidade natal<span style={{color:'red'}}>*</span></span>
+                        <div style={{ paddingBottom: '0.4rem' }}>
+                            <span className="span-state">Cidade natal<span style={{ color: 'red' }}>*</span></span>
                         </div>
                         <div className="padding-bottom-1">
                             <Select
-                            className="style-select-nationality"
-                            labelId="select-state"
-                            id="select-state"
-                            value={selectedState}
-                            onChange={handleChangeSelect}
+                                className="style-select-nationality"
+                                labelId="select-state"
+                                id="select-state"
+                                value={selectedState}
+                                onChange={handleChangeSelect}
                             >
                                 {states.map((state) => (
-                                <MenuItem key={state.id} value={state.nome}>
-                                    {state.nome}
-                                </MenuItem>
+                                    <MenuItem key={state.id} value={state.nome}>
+                                        {state.nome}
+                                    </MenuItem>
                                 ))}
                             </Select>
                         </div>
                     </div>
                     <div>
-                        <div style={{paddingBottom:'0.4rem'}}>
-                            <span className="span-state">Estado natal<span style={{color:'red'}}>*</span></span>
+                        <div style={{ paddingBottom: '0.4rem' }}>
+                            <span className="span-state">Estado natal<span style={{ color: 'red' }}>*</span></span>
                         </div>
                         <div className="padding-bottom-1">
                             <Select
-                            className="style-select-nationality"
-                            labelId="select-state"
-                            id="select-state"
-                            value={selectedState}
-                            onChange={handleChangeSelect}
+                                className="style-select-nationality"
+                                labelId="select-state"
+                                id="select-state"
+                                value={selectedState}
+                                onChange={handleChangeSelect}
                             >
                                 {states.map((state) => (
-                                <MenuItem key={state.id} value={state.nome}>
-                                    {state.nome}
-                                </MenuItem>
+                                    <MenuItem key={state.id} value={state.nome}>
+                                        {state.nome}
+                                    </MenuItem>
                                 ))}
                             </Select>
                         </div>
@@ -104,86 +105,118 @@ function Nationality () {
                 </div>
                 <div className="div-grid-nationality-inputs-1">
                     <div>
-                        <div style={{paddingBottom:'0.4rem'}}>
-                            <span className="span-state">Nacionaliadade</span>
+                        <div style={{ paddingBottom: '0.4rem' }}>
+                            <span className="span-state">Nacionaliadade<span style={{ color: 'red' }}>*</span></span>
                         </div>
                         <div className="padding-bottom-1">
                             <Select
-                            className="style-select-nationality-1"
-                            labelId="select-state"
-                            id="select-state"
-                            value={selectedState}
-                            onChange={handleChangeSelect}
+                                className="style-select-nationality-1"
+                                labelId="select-state"
+                                id="select-state"
+                                value={selectedState}
+                                onChange={handleChangeSelect}
                             >
                                 {states.map((state) => (
-                                <MenuItem key={state.id} value={state.nome}>
-                                    {state.nome}
-                                </MenuItem>
+                                    <MenuItem key={state.id} value={state.nome}>
+                                        {state.nome}
+                                    </MenuItem>
                                 ))}
                             </Select>
                         </div>
                     </div>
                     <div>
-                        <div style={{paddingBottom:'1rem'}}>
-                            <span className="span-state">Você possui outra nacionalidade?<span style={{color:'red'}}>*</span></span>
+                        <div style={{ paddingBottom: '1rem' }}>
+                            <span className="span-state">Você possui outra nacionalidade?<span style={{ color: 'red' }}>*</span></span>
                         </div>
                         <div className="padding-bottom-1">
-                        <RadioGroup
+                            <RadioGroup
                                 aria-labelledby="demo-radio-buttons-group-label"
                                 defaultValue="Sim"
                                 name="radio-buttons-group"
                                 className="subTitle-div-2"
-                                row         
-                                value={radioRequester}   
-                                onChange={handleChangeRequester}                    
+                                row
+                                value={radioRequester}
+                                onChange={handleChangeRequester}
                             >
                                 <FormControlLabel value="Sim" control={<Radio />} label="Sim" />
-                                <FormControlLabel value="Não" control={<Radio />} label="Não" />                                
-                        </RadioGroup>
+                                <FormControlLabel value="Não" control={<Radio />} label="Não" />
+                            </RadioGroup>
                         </div>
-                    </div>                
+                    </div>
+                </div>
+                <div className="div-grid-nationality-inputs-2">
+                    <div>
+                        <div style={{ paddingBottom: '0.4rem' }}>
+                            <span className="span-state">Insira sua foto<span style={{ color: 'red' }}>*</span></span>
+                        </div>
+                        <div className="margin-icon">
+                            <InsertPhotoOutlinedIcon sx={{ fontSize: 200 }} color="disabled" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="div-info-img">
+                            <div className="div-info-img-2">
+                                <div style={{ paddingBottom: '0.4rem' }}>
+                                    <span className="span-state">Sua foto deve ter</span>
+                                </div>
+                                <div>
+                                    <li className="inf-img">Rosto visivel, sem óculos, bonés ou obstruções faciais.</li>
+                                    <li className="inf-img">Fundo claro, preferencialmente branco, sem cores ou padrões.</li>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="div-bnt">
+                            <button
+                                type='button'
+                                className='button-style-img'                                
+                               
+                            >
+                                <span className='font-button'>{'Procurar'}</span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
             {radioRequester === "Sim" ? (
-            <div className="div-nationality-padding">
-                <div className="padding-bottom-nationality">
-                    <span className="title-header-2">Outras nacionalidades</span><br />
-                    <span className="subtitle-header">Caso possua outra nacionalidade ou cidadania, escolha o pais da mesma</span>
-                </div>
-                <div className="div-grid-nationality-inputs-1">
-                    <div>
-                        <div style={{paddingBottom:'0.4rem'}}>
-                            <span className="span-state">Nacionalidade<span style={{color:'red'}}>*</span></span>
+                <div className="div-nationality-padding">
+                    <div className="padding-bottom-nationality">
+                        <span className="title-header-2">Outras nacionalidades</span><br />
+                        <span className="subtitle-header">Caso possua outra nacionalidade ou cidadania, escolha o pais da mesma</span>
+                    </div>
+                    <div className="div-grid-nationality-inputs-1">
+                        <div>
+                            <div style={{ paddingBottom: '0.4rem' }}>
+                                <span className="span-state">Nacionalidade<span style={{ color: 'red' }}>*</span></span>
+                            </div>
+                            <div className="padding-bottom-1">
+                                <Select
+                                    className="style-select-nationality-1"
+                                    labelId="select-state"
+                                    id="select-state"
+                                    value={selectedState}
+                                    onChange={handleChangeSelect}
+                                >
+                                    {states.map((state) => (
+                                        <MenuItem key={state.id} value={state.nome}>
+                                            {state.nome}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </div>
                         </div>
-                        <div className="padding-bottom-1">
-                            <Select
-                            className="style-select-nationality-1"
-                            labelId="select-state"
-                            id="select-state"
-                            value={selectedState}
-                            onChange={handleChangeSelect}
-                            >
-                                {states.map((state) => (
-                                <MenuItem key={state.id} value={state.nome}>
-                                    {state.nome}
-                                </MenuItem>
-                                ))}
-                            </Select>
+                        <div>
+                            <div style={{ paddingBottom: '0.4rem' }}>
+                                <span className="span-state">Número do passaporte<span style={{ color: 'red' }}>*</span></span>
+                            </div>
+                            <div className="padding-bottom-1">
+                                <TextField id="outlined-basic" className="style-select-nationality-1" placeholder="Escreva o seu número do passaporte" variant="outlined" />
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <div style={{paddingBottom:'0.4rem'}}>
-                            <span className="span-state">Número do passaporte<span style={{color:'red'}}>*</span></span>
-                        </div>
-                        <div className="padding-bottom-1">
-                            <TextField id="outlined-basic" className="style-select-nationality-1" placeholder="Escreva o seu número do passaporte" variant="outlined" />
-                        </div>
-                    </div>
                 </div>
-            </div>
             ) : null}
-        </div>    
-  )
+        </div>
+    )
 }
 
 export default Nationality
